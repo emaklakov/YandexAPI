f43e91ae1ca9e0cee900388dadb82045209892bb
zyshchyk.maksim <amporioo@gmail.com>
2014-05-11 19:15:53 +0300

    Add Yandex Api Get Сoordinates

---
+++ YandexAPIGetСoordinates/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Autor: Zyshchyk Makism
+ * Email: amporioo@gmail.com
+ * Date: 11.05.2014
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexAPI.Maps;
+using YandexMap = YandexAPI.Request;
+
+namespace YandexAPIGetСoordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://geocode-maps.yandex.ru/1.x/?geocode=" + "Беларусь, Минск ул.Немига, 3" + "&results=1";
+            Console.WriteLine("Прямое геокодирование API Яндекс.Карт");
+            string result = YandexMap.Get(url);
+            PointD pointD = GeoCode.GetPointD(result);
+            Console.WriteLine(pointD);
+            Console.ReadKey(true);
+        }
+    }
+}
